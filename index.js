7c482af55e3ec09865b81f6b95a12d053d1d15c3
1JaydenTD500 <128873432+1JaydenTD500@users.noreply.github.com>
2023-04-28 07:27:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-var express = require('express'),
-    app = express(),
-    serv = require('http').Server(app);
-app.get('/', (_, res) => res.sendFile(__dirname + '/index.html'));
-app.get('/games', (_, res) => res.sendFile(__dirname + '/main/games.html'));
-
-['2048', 'snake', 'test', 'tetris', 'tictactoe', 'pacman'].forEach(game => {
-    app.get('/games/' + game, (_, res) => res.sendFile(__dirname + '/main/games/'+game+'.html'))
-})
-
-app.use(express.static('main'));
-
-serv.listen(process.env.PORT || 2000);
+document.getElementById('but_library').onclick = () => location.assign('games');
